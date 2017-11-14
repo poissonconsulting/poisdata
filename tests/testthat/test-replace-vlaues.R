68afa3e41775a5d9e8cebd39f4b76b1d1fd0f9bf
sebdalgarno <seb@poissonconsulting.ca>
2017-11-14 14:41:32 -0800

    fix ps_replace_values to work with sf object and single column change

--- tests/testthat/test-replace-vlaues.R
+++ tests/testthat/test-replace-vlaues.R
@@ -19,5 +19,9 @@ test_that("replaces values for columns with identical class to from vector", {
   # check that different to class does not change class of all columns
   xsf2 <- ps_replace_values(xsf, from = 6, to = "6")
   expect_is(xsf2$mpg, "numeric")
-
+  # check that when specify specific column with active sfc, error does not occur
+  xsf2 <- ps_replace_values(xsf, columns = c("cyl"), from = 6, to = NA_real_)
+  expect_is(xsf2$mpg, "numeric")
+  expect_identical(class(xsf$cylf), class(xsf2$cylf))
+  expect_true(any(is.na(xsf2$cyl)))
 })
