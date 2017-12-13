2b44e78e6557d120b357ac6fc684332b3a8f05d7
Joe Thorley <joe@poissonconsulting.ca>
2017-12-12 16:49:31 -0800

    add_missing_timeseries by

--- R/add-missing-timeseries.R
+++ R/add-missing-timeseries.R
@@ -7,6 +7,7 @@
 #'
 #' @param x The data.frame.
 #' @param datetime A string naming the POSIXt column.
+#' @param by A character vector of columns to add the missing datetimes by.
 #'
 #' @return A tbl data frame sorted by DateTime.
 #' @export
@@ -14,21 +15,35 @@
 #' @examples
 #' datetime <- as.POSIXct("2001-01-02 03:04:06") + c(1,3,9)
 #' ps_add_missing_datetimes(data.frame(DateTime = datetime, Value = c(1,3,9)))
-ps_add_missing_datetimes <- function(x, datetime = "DateTime") {
+ps_add_missing_datetimes <- function(x, datetime = "DateTime", by = character(0)) {
   check_string(datetime)
+  check_vector(by, "", unique = TRUE)
 
   values <- list(as.POSIXct("2001-01-02 03:04:05"))
   names(values) <- datetime
 
   check_data(x, values)
+  if(length(by)) check_colnames(x, by)
 
   x %<>% tibble::as_tibble()
 
   if(!nrow(x)) return(x)
 
-  new <- tibble::data_frame(DateTime = seq_datetime(x[[datetime]]))
-  colnames(new) <- datetime
-  x %<>% dplyr::left_join(new, ., by = datetime) %>%
-    dplyr::arrange(UQ(parse_quosure(datetime)))
+  # no bys
+  if(!length(by)) {
+    new <- tibble::data_frame(DateTime = seq_datetime(x[[datetime]]))
+    colnames(new) <- datetime
+    x %<>% dplyr::left_join(new, ., by = datetime) %>%
+      dplyr::arrange(UQ(parse_quosure(datetime)))
+    return(x)
+  }
+  # only one combination of bys
+  if(length(unique(plyr::id(x[by]))) == 1L) {
+    x %<>% ps_add_missing_datetimes(datetime = datetime)
+    x[,by] <- x[1L,by]
+    return(x)
+  }
+
+  x %<>% plyr::ddply(by, ps_add_missing_datetimes, datetime = datetime, by = by)
   x
 }
--- tests/testthat/test-add-missing-timeseries.R
+++ tests/testthat/test-add-missing-timeseries.R
@@ -2,15 +2,32 @@ context("add-missing-timeseries")
 
 test_that("add_missing_timeseries", {
   datetime <- as.POSIXct("2001-01-02 03:04:06") + c(1,3,9)
-  x <- ps_add_missing_datetimes(data.frame(DateTime = datetime, Value = c(1,3,9)))
+
+  data <- data.frame(DateTime = datetime, Value = c(1,3,9))
+  x <- ps_add_missing_datetimes(data)
   expect_is(x, "tbl")
   expect_identical(colnames(x), c("DateTime", "Value"))
   expect_identical(x$Value, c(1, 3, NA, NA, 9))
 
   datetime <- as.POSIXct("2001-01-02 03:04:06") + c(1,2,9)
-  x <- ps_add_missing_datetimes(data.frame(DateTimeReading = datetime, Value = c(1,3,9)), "DateTimeReading")
+  data <- data.frame(DateTimeReading = datetime, Value = c(1,2,9))
+
+  x <- ps_add_missing_datetimes(data, "DateTimeReading")
   expect_is(x, "tbl")
   expect_identical(colnames(x), c("DateTimeReading", "Value"))
   expect_identical(nrow(x), 9L)
+
+  datetime <- as.POSIXct("2001-01-02 03:04:06") + c(1,2,9,4)
+  data <- data.frame(DateTime = datetime, Value = c(1,2,9,4),
+                     Group = 1)
+
+  x <- ps_add_missing_datetimes(data, by = "Group")
+  expect_identical(x$Group, rep(1, 9L))
+
+  datetime <- as.POSIXct("2001-01-02 03:04:06") + c(1,2,9,4)
+  data <- data.frame(DateTime = datetime, Value = c(1,2,9,4),
+                     Group = c(1,1,1,3))
+  x <- ps_add_missing_datetimes(data, by = "Group")
+  expect_identical(x$Group, c(rep(1, 9L), 3))
 })
 
