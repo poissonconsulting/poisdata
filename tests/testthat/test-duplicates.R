b7b915ef0b6d3f00973c9180559a53f80de13921
Duncan Kennedy <duncan@poissonconsulting.ca>
2024-07-25 13:21:01 -0600

    updated `ps_duplicates()` tests so they still pass even after function is deprecated

--- tests/testthat/test-duplicates.R
+++ tests/testthat/test-duplicates.R
@@ -1,4 +1,6 @@
 test_that("can read multiple hobo csv file", {
+  rlang::local_options(lifecycle_verbosity = "quiet")
+
   tib <- dplyr::tibble(x = c(1, 2, 1), y = c(1, 1, 1))
   expect_identical(ps_duplicates(tib), tib[c(1, 3), ])
   expect_identical(ps_duplicates(as.data.frame(tib)), tib[c(1, 3), ])
@@ -10,5 +12,13 @@ test_that("can read multiple hobo csv file", {
   expect_equal(
     ps_duplicates(data.frame(x = c(1, 2, 1), y = 1:3), c("x", "y")),
     dplyr::tibble(x = double(0), y = double(0))
+  )
+})
+
+test_that("ps_duplicates() gives deprecation message", {
+  rlang::local_options(lifecycle_verbosity = "warning")
+  expect_warning(
+    ps_duplicates(data.frame(x = 1:2, y = 1:2)),
+    regexp = "was deprecated"
   )
 })
